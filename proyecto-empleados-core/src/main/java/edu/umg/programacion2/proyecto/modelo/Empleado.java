2710c50a72a053a5374ffde44ad3f16ad00515f7
Brandon Lopez <blopezt16@miumg.edu.gt>
2026-09-23 17:28:59 -0600

    feat: crear modelo Empleado con atributos y validaciones

---
+++ proyecto-empleados-core/src/main/java/edu/umg/programacion2/proyecto/modelo/Empleado.java
@@ -0,0 +1,14 @@
+package edu.umg.programacion2.proyecto.modelo;
+
+import java.time.LocalDate;
+
+public class Empleado {
+    private int id;
+    private String nombre;
+    private String departamento;
+    private LocalDate fechaContratacion;
+    private double salario;
+    private boolean activo;
+}
+
+public Empleado
